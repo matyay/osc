f3ce169723c6fa0598aafef08da3fad225174266
Maciej Kurc <mkurc1234@gmail.com>
2024-12-16 19:10:49 +0100

    Base OSC structures
    Signed-off-by: Maciej Kurc <mkurc1234@gmail.com>

---
+++ osc.c
@@ -0,0 +1,115 @@
+#include "osc.h"
+
+#include <string.h>
+
+// ============================================================================
+
+char* osc_strdup (const char* str) {
+    size_t len = strlen(str);
+    char*  res = (char*)osc_malloc(len + 1);
+
+    strcpy(res, str);
+    return res;
+}
+
+// ============================================================================
+
+OscMessage* osc_message_create (const char* tags) {
+
+    // Tag string cannot be NULL
+    if (tags == NULL) {
+        return NULL;
+    }
+
+    // Allocate the message
+    OscMessage* msg = (OscMessage*)osc_malloc(sizeof(OscMessage));
+    msg->addr = NULL;
+    msg->tags = osc_strdup(tags);
+    msg->next = NULL;
+
+    // Allocate & clear args
+    size_t asize = sizeof(OscArgument) * strlen(tags);
+    if (asize) {
+        msg->args = (OscArgument*)osc_malloc(asize);
+        memset((void*)msg->args, 0, asize);
+    }
+    else {
+        msg->args = NULL;
+    }
+
+    return msg;
+}
+
+OscMessage* osc_message_delete (const OscMessage* msg) {
+
+    if (!msg) {
+        return NULL;
+    }
+
+    // Free arguments (especially strings)
+    if (msg->args) {
+        for (size_t i=0; i<strlen(msg->tags); ++i) {
+            if (msg->tags[i] == 's' || msg->tags[i] == 'S') {
+                if (msg->args[i].str) {
+                    osc_free((void*)msg->args[i].str);
+                }
+            }
+        }
+
+        osc_free((void*)msg->args);
+    }
+
+    // Free tags string
+    if (msg->tags) {
+        osc_free((void*)msg->tags);
+    }
+
+    // Free address string
+    if (msg->addr) {
+        osc_free((void*)msg->addr);
+    }
+
+    // Free the message
+    osc_free((void*)msg);
+
+    return NULL;
+}
+
+// ============================================================================
+
+OscBundle* osc_bundle_create (int64_t timestamp) {
+
+    OscBundle* bundle = (OscBundle*)osc_malloc(sizeof(OscBundle));
+    bundle->timestamp = timestamp;
+    bundle->messages  = NULL;
+    bundle->bundles   = NULL;
+    bundle->next      = NULL;
+
+    return bundle;
+}
+
+OscBundle* osc_bundle_delete (const OscBundle* bundle) {
+
+    if (!bundle) {
+        return NULL;
+    }
+
+    // Delete member messages
+    for (const OscMessage* curr = bundle->messages; curr;) {
+        const OscMessage* next = curr->next;
+        osc_message_delete(curr);
+        curr = next;
+    }
+
+    // Delete member bundles
+    for (const OscBundle* curr = bundle->bundles; curr;) {
+        const OscBundle* next = curr->next;
+        osc_bundle_delete(curr);
+        curr = next;
+    }
+
+    // Delete self
+    osc_free((void*)bundle);
+
+    return NULL;
+}
---
+++ osc.h
@@ -0,0 +1,72 @@
+#ifndef OSC_H
+#define OSC_H
+
+#include <stdint.h>
+#include <stdlib.h>
+
+// ============================================================================
+
+// OSC "immediate" time tag
+#define OSC_IMMEDIATE 1LL
+
+// ============================================================================
+
+// OSC message argument
+typedef union _OscArgument {
+    int32_t i32;
+    float   f32;
+    int64_t i64;
+    double  f64;
+    char*   str;
+    uint8_t b[8];
+} OscArgument;
+
+// OSC message
+typedef struct _OscMessage {
+
+    char*           addr;   // Address string
+    char*           tags;   // Tag string
+    OscArgument*    args;   // Argument array (of length of the tag string)
+
+    struct _OscMessage* next;
+
+} OscMessage;
+
+// OSC bundle (linked list)
+typedef struct _OscBundle {
+
+    int64_t             timestamp;  // Timestamp
+    struct _OscMessage* messages;   // Messages
+    struct _OscBundle*  bundles;    // Sub-bundles
+
+    struct _OscBundle*  next;
+
+} OscBundle;
+
+// ============================================================================
+
+extern void* osc_malloc (size_t size);
+extern void  osc_free   (void* ptr);
+
+char* osc_strdup (const char* str);
+
+// ============================================================================
+
+OscMessage* osc_message_create  (const char* tags);
+OscMessage* osc_message_delete  (const OscMessage* msg);
+
+OscBundle* osc_bundle_create    (int64_t timestamp);
+OscBundle* osc_bundle_delete    (const OscBundle* bundle);
+
+// ============================================================================
+
+OscBundle* osc_parse (const uint8_t* data, size_t size);
+
+// ============================================================================
+
+int osc_encode_message (const OscMessage* msg, uint8_t** pdata, size_t* psize);
+int osc_encode_bundle (const OscBundle* bundle, uint8_t** pdata, size_t* psize);
+
+// ============================================================================
+
+#endif // OSC_H
